d6c81b1b4ae4cf5a93247e9ae1a54acab3380522
Valentin Sachenko <valentin181988@gmail.com>
2021-10-19 18:06:36 +0300

    last

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+//import * as basicLightbox from 'basiclightbox@5.0.4';
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryItemsCollection = galleryItems.map(({preview, original, description}) =>
+    `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+).join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsCollection);
+
+galleryContainer.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  console.log(event.target)
+
+  /* const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${event.target.dataset.source}"/>
+    </div>
+`)
+
+instance.show() */
+}) 
+
+
+
+
+
+

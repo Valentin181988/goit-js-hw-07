4abda2da41f1a5cc124d1c86775e6126c03155cc
Valentin Sachenko <valentin181988@gmail.com>
2021-10-20 16:15:57 +0300

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryItemsCollection = galleryItems.map(({preview, original, description}) =>
+`<a class="gallery__item" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+).join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsCollection);
